52614c34026d362e647c3b0d926940d723ee8d98
hamza moalla <120567169+hamzamoalla@users.noreply.github.com>
2024-09-23 20:20:12 +0100

    Update k8s-deployment.sh

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -2,7 +2,8 @@
 
 #k8s-deployment.sh
 
-sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
+sed -i 's|image: hamzamoalla/my_repo:devsecops-.*|image: ${IMAGE_NAME}:${IMAGE_TAG}|' k8s_deployment_service.yaml
+
 # kubectl -n default get deployment ${deploymentName} > /dev/null
 
 # if [[ $? -ne 0 ]]; then
