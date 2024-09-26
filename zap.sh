6e837e1ccaf531835cad32c621b6349fe53a7a0e
hamza moalla <120567169+hamzamoalla@users.noreply.github.com>
2024-09-26 01:06:16 +0100

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -8,7 +8,10 @@ chmod 777 $(pwd)
 echo $(id -u):$(id -g)
 
 # Exécuter OWASP ZAP avec des règles personnalisées
-docker run -t ghcr.io/zaproxy/zaproxy:weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
+docker run -it --rm ghcr.io/zaproxy/zaproxy:weekly /zap/bin/zap-bash -c "
+    cd /zap/wrk
+    zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap_rules -r zap_report.html
+"
 
 # Récupérer le code de sortie de l'exécution de Docker
 exit_code=$?
@@ -20,12 +23,11 @@ mv zap_report.html owasp-zap-report
 echo "Exit Code : $exit_code"
 
 # Vérifier si le scan ZAP a trouvé des vulnérabilités
-if [[ ${exit_code} -ne 0 ]]; then
-    echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
-    exit 1
-else
-    echo "OWASP ZAP did not report any Risk"
-fi
+case $exit_code in
+    0) echo "OWASP ZAP did not report any Risk";;
+    1) echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report";;
+    *) echo "An unexpected error occurred during ZAP execution."; exit 1;;
+esac
 
 # Générer un fichier de configuration si nécessaire (ligne commentée)
 # docker run -v $(pwd):/zap/wrk/:rw -t zaproxy/zap-stable zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -g gen_file
