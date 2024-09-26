6b62615caacdc0714e4f8e90eff9e0d838b575f4
hamza moalla <120567169+hamzamoalla@users.noreply.github.com>
2024-09-26 01:30:56 +0100

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -8,27 +8,21 @@ chmod 777 $(pwd)
 echo $(id -u):$(id -g)
 
 # Exécuter OWASP ZAP avec des règles personnalisées
-docker run --rm ghcr.io/zaproxy/zaproxy:latest /zap/bin/zap-bash -c "
+docker run -it --rm ghcr.io/zaproxy/zaproxy:weekly /zap/bin/zap-bash -c "
     cd /zap/wrk
     zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap_rules -r zap_report.html
 "
-sleep 5
-# Vérifier si le scan ZAP a trouvé des vulnérabilités
+
+# Récupérer le code de sortie de l'exécution de Docker
 exit_code=$?
 
 # Créer un répertoire pour stocker le rapport ZAP
 mkdir -p owasp-zap-report
-
-if [ ! -f "/zap/wrk/zap_report.html" ]; then
-    echo "Le fichier raport.html n'a pas été généré dans le conteneur."
-    exit 1
-fi
-
-# Copier le rapport du conteneur vers l'hôte
-docker cp $(docker ps -q -f ancestor=ghcr.io/zaproxy/zaproxy:latest):/zap/wrk/zap_report.html ./owasp-zap-report/
+mv zap_report.html owasp-zap-report
 
 echo "Exit Code : $exit_code"
 
+# Vérifier si le scan ZAP a trouvé des vulnérabilités
 case $exit_code in
     0) echo "OWASP ZAP did not report any Risk";;
     1) echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report";;
