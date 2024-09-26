5a86473fe7bf90ab6a6b1972aa54ee0a449d98fa
hamza <moallahamza@gmail.com>
2024-09-26 02:20:57 +0100

    prod1

--- integration-test-PROD.sh
+++ integration-test-PROD.sh
@@ -2,10 +2,10 @@
 sleep 5s
 
 # echo "ok"
-# PORT=$(kubectl get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
+PORT=$(kubectl -n prod get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
 
-### Istio Ingress Gateway Port 80 - NodePort
-PORT=$(kubectl -n istio-system get svc istio-ingressgateway -o json | jq '.spec.ports[] | select(.port == 80)' | jq .nodePort)
+# Istio Ingress Gateway Port 80 - NodePort
+# PORT=$(kubectl -n istio-system get svc istio-ingressgateway -o json | jq '.spec.ports[] | select(.port == 80)' | jq .nodePort)
 
 
 echo $PORT
